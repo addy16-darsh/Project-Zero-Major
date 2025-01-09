3cddd7420cbd7b41c619383b9f7df6ea82efcd5a
Addy <ajaykumaradarsh80@gmail.com>
2025-01-09 22:47:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -154,7 +154,6 @@ def upload_file():
     return render_template('upload.html')
 
 if __name__ == '__main__':
-    # Comment out the app.run() line as Gunicorn will run the app in production
-    pass
+    app.run(debug=True, host='0.0.0.0', port=5000)
 
 
